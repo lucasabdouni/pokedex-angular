670bb79a92d438362d7434a57c84250d12970071
lucasabdouni <lucasabdouni@hotmail.com>
2024-01-23 12:12:58 -0300

    feat: created shared module for component and create header and search components

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 //Module Routing
 import { RoutingModule } from './routing.modules';
 
+// Modules
+import { SharedModule } from '../shared/shared.module';
+
 // Components
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
@@ -15,7 +18,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
